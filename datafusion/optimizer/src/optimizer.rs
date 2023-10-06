080f83a2a3a9f93cdfadf7507acba7146278e115
Oleg Utkin <oleg_utkin@me.com>
2023-10-06 15:25:18 +0200

    fix generate_different_schema test

--- datafusion/optimizer/src/optimizer.rs
+++ datafusion/optimizer/src/optimizer.rs
@@ -488,8 +488,6 @@ mod tests {
     }
 
     #[test]
-    // TODO: remove ignore and fix the test
-    #[ignore]
     fn generate_different_schema() {
         let opt = Optimizer::with_rules(vec![Arc::new(GetTableScanRule {})]);
         let config = OptimizerContext::new().with_skip_failing_rules(false);
@@ -501,11 +499,15 @@ mod tests {
         assert_eq!(
             "Optimizer rule 'get table_scan rule' failed\ncaused by\nget table_scan rule\ncaused by\n\
              Internal error: Failed due to generate a different schema, \
-             original schema: DFSchema { fields: [], metadata: {}, functional_dependencies: FunctionalDependencies { deps: [] } }, \
+             original schema: DFSchema { fields: [], fields_index: {}, metadata: {}, functional_dependencies: FunctionalDependencies { deps: [] } }, \
              new schema: DFSchema { fields: [\
              DFField { qualifier: Some(Bare { table: \"test\" }), field: Field { name: \"a\", data_type: UInt32, nullable: false, dict_id: 0, dict_is_ordered: false, metadata: {} } }, \
              DFField { qualifier: Some(Bare { table: \"test\" }), field: Field { name: \"b\", data_type: UInt32, nullable: false, dict_id: 0, dict_is_ordered: false, metadata: {} } }, \
              DFField { qualifier: Some(Bare { table: \"test\" }), field: Field { name: \"c\", data_type: UInt32, nullable: false, dict_id: 0, dict_is_ordered: false, metadata: {} } }], \
+             fields_index: {\
+             FieldQ { field: \"a\", table: Some(\"test\"), schema: None, catalog: None }: 0, \
+             FieldQ { field: \"b\", table: Some(\"test\"), schema: None, catalog: None }: 1, \
+             FieldQ { field: \"c\", table: Some(\"test\"), schema: None, catalog: None }: 2}, \
              metadata: {}, functional_dependencies: FunctionalDependencies { deps: [] } }.\
              \nThis was likely caused by a bug in DataFusion's code \
              and we would welcome that you file an bug report in our issue tracker",
